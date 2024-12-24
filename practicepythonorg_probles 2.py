d0d2df3c40e6eb338dcbf5ae270562d1f6e24918
Dhanonjoy Saha Anogh <104628129+DjSahaAnogh@users.noreply.github.com>
2024-12-24 20:04:03 +0600

    20th question done

--- practicepythonorg_probles 2.py
+++ practicepythonorg_probles 2.py
@@ -17,6 +17,39 @@ title = soup.title.string
 
 article = soup.find_all("p")
 x: int = 0
-for i in article:
-  x += 1
-  print(f"{x}:\n {i.get_text()}\n")
+# for i in article:
+#   x += 1
+#   print(f"{x}:\n {i.get_text()}\n")
+
+# 19.  Write a function that takes an ordered list of numbers and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+
+def check_list(num_list: list, num: int) -> bool:
+  if num in num_list:
+    return True
+  else:
+    return False
+  
+# print(check_list([1, 2, 3, 4, 5], 3))
+
+# or
+
+def binary_search(arr: list, target: int) -> bool:
+    low: int = 0
+    high: int = len(arr) - 1
+    
+    while low <= high:
+        mid: int = (low + high) // 2 
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if low > high:
+        return False
+    
+# print(binary_search([1, 2, 3, 4, 5], 3))
+
+# 20. Write To A File
+with open("emaple.txt", "w") as text:
+    text.write("hello")
