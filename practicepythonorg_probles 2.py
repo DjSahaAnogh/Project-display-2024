00add89b4f467a0403f9c871e98bd6a20a29ba0f
Dhanonjoy Saha Anogh <104628129+DjSahaAnogh@users.noreply.github.com>
2024-12-24 18:25:16 +0600

    18th question done

--- practicepythonorg_probles 2.py
+++ practicepythonorg_probles 2.py
@@ -4,3 +4,19 @@
 # URL OF THE WEBSITE https://www.practicepython.org/
 
 # ALL IMPORTS
+import requests
+from bs4 import BeautifulSoup
+
+# 18. Decode A Web Page Two
+
+url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+responce = requests.get(url)
+
+soup = BeautifulSoup(responce.text, "html.parser")
+title = soup.title.string
+
+article = soup.find_all("p")
+x: int = 0
+for i in article:
+  x += 1
+  print(f"{x}:\n {i.get_text()}\n")
